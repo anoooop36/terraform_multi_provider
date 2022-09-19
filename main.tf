3bbb4d5211a22533edc0a720b3442ac81301417a
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-09-19 16:17:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source = "lightstep/lightstep"
+      version = "1.61.1"
+    }
+  }
+}
+
+provider "lightstep" {
+  api_key         = "your-lightstep-org-api-key"
+  organization    = "your-lightstep-organization"
+}
+
+# Example: Create AWS EC2 Dashboard
+module "aws-dashboards" {
+  source            = "lightstep/aws-dashboards/lightstep//modules/ec2-dashboard"
+  lightstep_project = "your-lightstep-project"
+}
+
+provider "aws" {
+ access_key = var.access_key
+ secret_key = var.secret_key
+ region     = var.region
+}
+
+resource "aws_ebs_volume" "awsEbsExampleWithList" {
+  availability_zone = "us-east-1a"
+  size              = var.ebsSizes[0]
+  tags              = {
+                        Name = var.sampleTags[1]
+                      }
+}
+
+
+
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+ type    = string
+ default = "us-east-1"
+}
+
+variable "sampleTags" {
+  default = ["Dev ebs volume with List", "Test ebs volume with List", "Prod ebs volume with List"]
+  type = list(string)
+}
+
+variable "ebsSizes" {
+  type = list(number)
+  default = [40, 80, 120]
+}
