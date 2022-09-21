b8a8dcf31b1aff2df863687f9a7933026d34c991
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-09-22 01:12:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,12 @@ provider "aws" {
  region     = var.region
 }
 
+provider "azurerm" {
+ access_key = var.access_key
+ secret_key = var.secret_key
+ region     = var.region
+}
+
 provider "aws" {
  access_key = var.access_key
  secret_key = var.secret_key
