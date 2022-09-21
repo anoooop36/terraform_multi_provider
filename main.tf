81497dde9db6cbabafa73e8bfcdb367962a06324
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-09-22 01:11:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,18 @@ terraform {
   }
 }
 
+provider "aws" {
+ access_key = var.access_key
+ secret_key = var.secret_key
+ region     = var.region
+}
+
+provider "aws" {
+ access_key = var.access_key
+ secret_key = var.secret_key
+ region     = var.region
+}
+
 
 provider "lightstep" {
   api_key         = "your-lightstep-org-api-key"
