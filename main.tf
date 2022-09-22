d0beab3a09667a924e231a02f6a4e18f2e747280
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-09-22 09:59:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,19 +7,8 @@ terraform {
   }
 }
 
-provider "aws" {
- access_key = var.access_key
- secret_key = var.secret_key
- region     = var.region
-}
 
 provider "azurerm" {
- access_key = var.access_key
- secret_key = var.secret_key
- region     = var.region
-}
-
-provider "aws" {
  access_key = var.access_key
  secret_key = var.secret_key
  region     = var.region
